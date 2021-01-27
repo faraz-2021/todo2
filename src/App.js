2884abd9f759ae3e3e698cb337080124c1f5266d
faraz uddin <faraz@excellencetechnologies.in>
2021-01-27 16:10:35 +0530

    faraz

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './todo';
+
+function App() {
+  return (
+    <div style={{display:"flex",justifyContent:"center"}}>
+     <Todo />
+    </div>
+  );
+}
+
+export default App;
